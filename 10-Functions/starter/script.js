e694ad72b3d9af5eb41c8d518c46c28211176ed4
arunsammit <arunsammit@gmail.com>
2021-05-24 10:45:37 +0530

    coding challange 4 section 9

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,7 @@
 'use strict';
+const greet = greeting => (name => console.log(`${ greeting } ${ name }`));
+greet('Hello')('world!');
+const addTax = rate => value => value * (1 + rate);
+const addVAT = addTax(.23);
+console.log(addVAT(100));
+console.log(addVAT(23));
